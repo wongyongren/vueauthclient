e64e606b291929bc19d7bf225a468d7c27f629fc
wongyongren <wongyongren@hotmail.com>
2022-01-15 14:41:16 +0800

    commit for user edit

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,16 +1,27 @@
 <template>
   <v-container>
-    <h1 style="padding-top: 100px; padding-bottom: 100px">
-      Edit User Page
-    </h1>
+    <h1 style="padding-top: 100px; padding-bottom: 100px">Edit User Page</h1>
     <v-spacer></v-spacer>
-    <v-form v-model="valid" v-on:submit="updateemploeyee">
+    <v-form v-if="role === 'admin'" v-on:submit="adminupdateuser">
       <v-row>
         <v-col cols="12" sm="10" md="8" lg="6">
           <v-text-field
-            v-model="employee_name"
+            v-model="user_name"
             :rules="usernameRules"
             name="username"
+            label="Employee User Name"
+            autocomplete="off"
+            required
+            disabled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="userdisplay_name"
+            :rules="usernameRules"
+            name="name"
             label="Employee Name"
             autocomplete="off"
             required
@@ -20,23 +31,122 @@
       <v-row>
         <v-col cols="12" sm="10" md="8" lg="6">
           <v-select
-            v-model="worker_name"
-            :items="workernameOption"
-            name="workername"
-            item-value="userid"
-            item-text="name"
-            label="Login User Name"
+            v-model="roles"
+            :items="selectOption"
+            :menu-props="{ maxHeight: '400' }"
+            name="role"
+            label="Role"
             persistent-hint
+            required
           ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="password"
+            :append-icon="passwordshow ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="passwordRules"
+            :type="passwordshow ? 'text' : 'password'"
+            name="password"
+            label="Password"
+            hint="At least 6 characters"
+            autocomplete="off"
+            counter
+            @click:append="passwordshow = !passwordshow"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="confirmpassword"
+            :append-icon="confirmpasswordshow ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="confirmPasswordRules.concat(passwordConfirmationRule)"
+            :type="confirmpasswordshow ? 'text' : 'password'"
+            name="confirmpassword"
+            label="Confirm Password"
+            hint="At least 6 characters"
+            autocomplete="off"
+            counter
+            @click:append="confirmpasswordshow = !confirmpasswordshow"
+            required
+          ></v-text-field>
         </v-col>
       </v-row>
 
       <v-row>
-        <v-btn
-          :disabled="!valid"
-          class="loginButton"
-          type="submit"
-          value="updateemploeyee"
+        <v-btn class="loginButton" type="submit" value="updateemploeyee"
+          >Submit</v-btn
+        >
+      </v-row>
+    </v-form>
+
+    <v-form v-if="role !== 'admin'" v-on:submit="updateuser">
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="user_name"
+            :rules="usernameRules"
+            name="username"
+            label="Employee User Name"
+            autocomplete="off"
+            required
+            disabled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="userdisplay_name"
+            :rules="usernameRules"
+            name="name"
+            label="Employee Name"
+            autocomplete="off"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="password"
+            :append-icon="passwordshow ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="passwordRules"
+            :type="passwordshow ? 'text' : 'password'"
+            name="password"
+            label="Password"
+            hint="At least 6 characters"
+            autocomplete="off"
+            counter
+            @click:append="passwordshow = !passwordshow"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-text-field
+            v-model="confirmpassword"
+            :append-icon="confirmpasswordshow ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="confirmPasswordRules.concat(passwordConfirmationRule)"
+            :type="confirmpasswordshow ? 'text' : 'password'"
+            name="confirmpassword"
+            label="Confirm Password"
+            hint="At least 6 characters"
+            autocomplete="off"
+            counter
+            @click:append="confirmpasswordshow = !confirmpasswordshow"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-btn class="loginButton" type="submit" value="updateemploeyee"
           >Submit</v-btn
         >
       </v-row>
@@ -50,18 +160,42 @@ import axios from "axios";
 
 export default {
   methods: {
-    updateemploeyee: function () {
-      let employeename = this.employee_name;
-      let userid = this.worker_name;
-      let employeeid = this.workerid;
+    updateuser: function () {
+      let name = this.userdisplay_name;
+      let password = this.password;
+      let userid = this.userid;
+
       let data = {
-        employeename: employeename,
+        name: name,
+        password: password,
         userid: userid,
-        employeeid: employeeid,
       };
       console.log(data);
       axios
-        .post("/api/updateemployee", data)
+        .post("/api/updateuser", data)
+        .then((response) => {
+          console.log("register");
+        })
+        .catch((errors) => {
+          console.log("Cannot Register");
+          console.log(errors);
+          alert("Duplicate User Name");
+        });
+    },
+    adminupdateuser: function () {
+      let name = this.userdisplay_name;
+      let password = this.password;
+      let roles = this.roles;
+      let userid = this.userid;
+      let data = {
+        name: name,
+        password: password,
+        role: roles,
+        userid: userid,
+      };
+      console.log(data);
+      axios
+        .post("/api/adminupdateuser", data)
         .then((response) => {
           console.log("register");
         })
@@ -72,7 +206,7 @@ export default {
         });
     },
     getWorkerData: function () {
-      this.workerid = this.$route.params.userid;
+      this.userid = this.$route.params.userid;
       let self = this;
       let data = {
         userid: this.$route.params.userid,
@@ -80,8 +214,9 @@ export default {
       axios
         .post("/api/displayuser", data)
         .then((response) => {
-          self.employee_name = response.data.username;
-          self.worker_name = response.data.userid;
+          self.userdisplay_name = response.data.name;
+          self.user_name = response.data.username;
+          self.roles = response.data.role;
         })
         .catch((errors) => {
           console.log("Cannot Register");
@@ -89,32 +224,33 @@ export default {
           alert("Duplicate User Name");
         });
     },
-    getLoginUserData: function () {
-      let self = this;
-      axios
-        .get("/api/workername")
-        .then((response) => {
-          for (let i = 0; i < response.data.length; i++) {
-            self.workernameOption.push(response.data[i]);
-          }
-        })
-        .catch((errors) => {
-          console.log("Cannot Register");
-          console.log(errors);
-          alert("worker name list error");
-        });
+  },
+  computed: {
+    passwordConfirmationRule() {
+      return () =>
+        this.password === this.confirmpassword || "Password must match";
     },
   },
   mounted() {
-    this.getLoginUserData();
     this.getWorkerData();
   },
   data: () => ({
-    worker_name: [],
-    workernameOption: [],
-    workerid: null,
-    valid: false,
-    employee_name: "",
+    selectOption: ["User", "Supervisor", "Admin"],
+    role: "admin",
+    roles:"",
+    password: null,
+    confirmpassword: null,
+    passwordRules: [
+      (v) => v.length >= 6 || "Password must at least 6 characters",
+    ],
+    confirmPasswordRules: [
+      (v) => v.length >= 6 || "Password must at least 6 characters",
+    ],
+    confirmpasswordshow: false,
+    passwordshow: false,
+    user_name: [],
+    userid: null,
+    userdisplay_name: "",
     usernameRules: [
       (v) => !!v || "User Name is required",
       // (v) => v.length <= 10 || "Name must be less than 6 characters",
